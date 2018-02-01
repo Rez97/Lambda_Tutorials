65b6c214f9614bed615164432451ed9444e79898
Rezhnd Raveendrakumar <raveere1@vasco.com>
2018-02-01 10:44:29 -0500

    Another commit

--- Streams.java
+++ Streams.java
@@ -2,6 +2,7 @@ package Unit3Package;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import java8lambdabasics.person;
 
@@ -20,8 +21,21 @@ public class Streams {
 		//create an assembly line to perform the actions.
 		//As of java 8, every object comes with a stream method.
 		
-		people.stream(); //based in the analogy it takes on the list of the people
+		people.stream()
+		.filter(p ->p.getLastname().startsWith("C"))  //this takes on a condition
+		.forEach(p -> System.out.println(p.getFirstName()));; //based in the analogy it takes on the list of the people
+		//stream takes the people list and creates a assembly chain.
 		
+		
+		
+		//stream is composed of three different elements
+		//1) source (collections)
+		//2) operation that need to be performed on the stream
+		//3) terminal conditions (end condition)
+		
+	    long count = people.stream().filter(p ->p.getLastname().startsWith("C")).count();
+	    
+	    System.out.println(count);
 		
 	}
 
